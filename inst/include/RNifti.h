f8b4d7311a3966252ab44a0dca4d6b8f28fd59a1
Anna <fpm.pankova@bsu.by>
2019-07-16 17:40:16 +0300

    my version

---
+++ inst/include/RNifti.h
@@ -0,0 +1,31 @@
+#ifndef _RNIFTI_H_
+#define _RNIFTI_H_
+
+// RNiftyReg and divest have used HAVE_R, so accept this variant for compatibility
+#if !defined(USING_R) && defined(HAVE_R)
+#define USING_R
+#endif
+
+// Defined since RNifti v0.10.0
+// Equal to 100 * (major version) + (minor version)
+// Does not change with patch level, since the API should not change
+#define RNIFTI_VERSION 11
+
+#include "niftilib/nifti1_io.h"
+
+#ifdef __cplusplus
+#include "lib/NiftiImage.h"
+
+// Defined since RNifti v0.3.0
+#define HAVE_RNIFTI_NAMESPACE
+
+extern "C" {
+#endif
+
+extern void niftilib_register_all ();
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif
