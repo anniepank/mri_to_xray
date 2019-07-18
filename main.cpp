6167507ace0096a96067de973f8898a257950ad6
Anna <fpm.pankova@bsu.by>
2019-07-18 09:00:57 +0300

    result

--- main.cpp
+++ main.cpp
@@ -14,9 +14,13 @@
 
 using namespace std;
 
+float interpolate(float x, float a, float b) {
+    return (b - a) * x + a;
+}
+
 int main(int argc, char *argv[]) {
 
-    nifti_image *nim_input = NULL;
+    nifti_image *nim_input = NULL, *nim_output = NULL;
     char *fin = NULL, *fout = NULL;
     int cutoff, ac, disp_float_eg = 0;
 
@@ -24,6 +28,18 @@ int main(int argc, char *argv[]) {
     // read input dataset, including data
 
     nim_input = nifti_image_read(fin, 1);
+
+    int dim[8] = {2, 512, 512, 1, 1, 0, 0, 0};
+    nim_output = nifti_make_new_nim(dim, DT_SIGNED_SHORT, true);
+    nim_output->dx = 1;
+    nim_output->dy = 1;
+    nim_output->dz = 1;
+    nim_output->dv = 0;
+    nim_output->du = 0;
+    nim_output->dt = 0;
+    nim_output->dw = 0;
+
+
     if (!nim_input) {
         fprintf(stderr, "** failed to read NIfTI image from '%s'\n", fin);
         return 2;
@@ -44,78 +60,74 @@ int main(int argc, char *argv[]) {
 
     // get access to data of nim_input
     auto nim_input_data = (int16_t *) nim_input->data;
-/*
- for (int islice = 0; islice < sizeSlice; ++islice) {
-        for (int iy = 0; iy < sizePhase; ++iy) {
-            for (int ix = 0; ix < sizeRead; ++ix) {
-                if (*(nim_input_data + nxy * islice + nx * ix + iy)  > 0)
-                auto a = *(nim_input_data + nxy * islice + nx * ix + iy);
-                        /* nim_input_data + nxyz * timestep + nxy * islice + nx * ix + iy *
-            }
-        }
-    }
-*/
-
     png::image<png::rgb_pixel> image(512, 512);
-    float min = 0;
-    float max = -2000;
+
+    int _min = -1024;
+    int _max = 2438;
+
     for (size_t y = 0; y < image.get_height(); ++y) {
         for (size_t x = 0; x < image.get_width(); ++x) {
             float result = 0;
+            float rayStrength = 1;
+            for (size_t v = 0; v < 512; v++) {
 
-            for (size_t z = 0; z < 512; z++) {
-                int value =  *(nim_input_data + nxy * y * (123 / 512) + nx * z + x);
-                //cout << value << endl;
-                if (value > -100) {
-                    cout << value << endl;
+                int value1, value2;
+
+                if ((nxy * (int16_t) ceil(y / (nim_input->dz / nim_input->dx))) +
+                    nx * (int16_t) ceil(v / (nim_input->dy / nim_input->dx)) + x >= 512 * 512 * 123 ) {
+                    value1 = 0;
+                    value2 = 0;
+                } else {
+
+                    value1 = *(nim_input_data +
+                               (nxy * (int16_t) floor(y / (nim_input->dz / nim_input->dx))) +
+                               nx * (int16_t) floor(v / (nim_input->dy / nim_input->dx)) + x);
+
+                    value2 = *(nim_input_data +
+                               (nxy * (int16_t) (ceil(y / (nim_input->dz / nim_input->dx)))) +
+                               nx * (int16_t) floor(v / (nim_input->dy / nim_input->dx)) + x);
+
                 }
-                if (value > max) {
-                    max = value;
+
+                float interpX = y * (nim_input->dz / nim_input->dx);
+
+                float interpolated = interpolate(interpX - floor(interpX), value1, value2);
+                float value = interpolated;
+
+                if (value <= -1000) {
+                    value = -1000;
                 }
-                if (value < min) {
-                    min = value;
-                }
 
-
-                value = std::max(-1000, value);
-                float fv = ((float)value + 1000) / 2300;
-                fv = std::min(fv, (float)1.0);
-                float opacity = fv / 300;
-
-                result = result * (1 - opacity) + 1 * opacity;
+                float normalized = (value - _min) / (_max - _min);
+                normalized *= normalized ;
+                rayStrength = rayStrength * (1 - normalized / 40);
+                // result = result * (1 - opacity) + 1 *  opacity;
             }
 
-            int ir = (int)(result);
-            if (result != 0) {
-                cout << result << endl;
-            }
-            image[y][x] = png::rgb_pixel(ir, ir, ir);
-            // non-checking equivalent of image.set_pixel(x, y, ...);
+            result = 1 - rayStrength;
+            int ir = (int) (result * 256);
+
+            image[511 - y][x] = png::rgb_pixel(ir, ir, ir);
+            ((int16_t*)nim_output->data)[512 * y + x] = (int16_t) ir;
+
         }
     }
 
-    cout << min << " " << max << endl;
-//    image.write("my_rgb.png");
+    nim_output->fname = (char *) "output.nii";
+    nim_output->iname = (char *) "output.nii";
+
+    nifti_image_write(nim_output);
+
+    image.write("rgb.png");
 
     return 0;
 }
 
-
-
-
-/* for (size_t v = 0; v < 512; v++) {
-                int value = *(nim_input_data + nxy * (y / 5) + nx * v + x);
-                value = std::max(-1000, value);
+/*
+ * value = std::max(-1000, value);
                 float fv = ((float)value + 1000) / 2300;
                 fv = std::min(fv, (float)1.0);
-                float opacity = fv / 300;
+                float opacity = fv / 500;
 
                 result = result * (1 - opacity) + 1 * opacity;
-
-    }
-
-            int ir = (int)(result * 256);
-            image[511-y][x] = png::rgb_pixel(ir, ir, ir);
-            // non-checking equivalent of image.set_pixel(x, y, ...);
-
- */
+                */
